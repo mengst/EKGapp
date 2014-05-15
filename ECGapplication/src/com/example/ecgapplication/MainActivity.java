92151d0150ef76d5669851081c14560481c8cb41
Magnus Engstrom <mengst@kth.se>
2014-05-15 14:34:55 +0200

    Added actionbar and 3 tabs

--- ECGapplication/src/com/example/ecgapplication/MainActivity.java
+++ ECGapplication/src/com/example/ecgapplication/MainActivity.java
@@ -1,47 +1,68 @@
 package com.example.ecgapplication;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+//import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+	
+	ActionBar.Tab tab1, tab2, tab3;
+	FragmentTab1 fragmentTab1 = new FragmentTab1();
+	FragmentTab2 fragmentTab2 = new FragmentTab2();
+	FragmentTab3 fragmentTab3 = new FragmentTab3();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_tab_test);
+		
+		android.app.ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(actionBar.NAVIGATION_MODE_TABS);
+		
+		tab1 = actionBar.newTab().setText("Main");
+		tab2 = actionBar.newTab().setText("ECG");
+        tab3 = actionBar.newTab().setText("Info");
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+        tab1.setTabListener(new MyTabListener(fragmentTab1));
+        tab2.setTabListener(new MyTabListener(fragmentTab2));
+        tab3.setTabListener(new MyTabListener(fragmentTab3));
+        
+        
+        actionBar.addTab(tab1);
+        actionBar.addTab(tab2);
+        actionBar.addTab(tab3);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		// Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		// Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        
+	        case R.id.action_settings:
+	            //openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 	/**
